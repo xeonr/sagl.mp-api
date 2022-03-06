cd526774bd257a5303cce27fb9228eee2e513b21
Luke Thompson <luke@xeonr.io>
2022-03-06 01:24:40 +0000

    fix: fix build

--- src/util/DB.ts
+++ src/util/DB.ts
@@ -1,7 +1,12 @@
 import config from 'config';
 import { Sequelize } from 'sequelize-typescript';
 
+import { GameServerBlacklist } from '../models/GameServerBlacklist';
+import { GameServerPing } from '../models/GameServerPing';
+import { GameServer } from './../models/GameServer';
+
 export const db = new Sequelize({
 	...config.get('db'),
-	models: [`${__dirname}/../models`],
 }); // tslint:disable-line
+
+db.addModels([GameServer, GameServerBlacklist, GameServerPing]);

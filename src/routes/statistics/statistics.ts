5b772183aea85f209991c86e3e31d6bc58cf91cd
Luke Thompson <luke@xeonr.io>
2022-06-19 19:59:27 +0100

    feat: add player and geo statistic routes

--- src/routes/statistics/statistics.ts
+++ src/routes/statistics/statistics.ts
@@ -1,9 +1,14 @@
+import { influxdb } from './../../util/Influxdb';
+import { flux } from '@influxdata/influxdb-client';
 import { AggregationsStringTermsAggregate } from '@elastic/elasticsearch/lib/api/types';
-import { Lifecycle, Server } from '@hapi/hapi';
+import { Lifecycle, Server, Request } from '@hapi/hapi';
 import config from '@majesticfudgie/vault-config';
+import { LocationRecord } from '@maxmind/geoip2-node';
 
 import { elasticsearch } from '../../util/Elasticsearch';
+import { lookupIP } from '../../util/MaxMind';
 import { RouterFn } from './../../util/Types';
+import { groupBy } from 'lodash';
 
 async function aggregate(property: string): Promise<{ key: string; value: number }[]> {
 	const results = await elasticsearch.search({
@@ -32,6 +37,78 @@ async function aggregate(property: string): Promise<{ key: string; value: number
 }
 
 export const routes: RouterFn = (router: Server): void => {
+	router.route({
+		method: 'GET',
+		path: '/statistics/geo',
+		handler: async (request: Request): Promise<Lifecycle.ReturnValue> => {
+			const results = await elasticsearch.search({
+				index: config.get('elasticsearch.index'),
+				"query": {
+					"bool": {
+						"must": [
+							{ "range": { "lastOnlineAt": { "gte": "now-12h/d" } } },
+							{
+								"exists": {
+									"field": "rules"
+								}
+							},
+							{
+								"exists": {
+									"field": "ipLocation"
+								}
+							}
+						]
+					}
+				},
+				"sort": [
+					{
+						"_geo_distance": {
+							"ipLocation": {
+								"lat": 52.9044,
+								"lon": -1.2326
+							},
+							"order": "asc",
+							"unit": "mi"
+						}
+					},
+				] as any,
+				"fields": [
+					"hostname",
+					"ipLocation",
+					"onlinePlayers",
+					"maxPlayers",
+					"country"
+				],
+				"_source": false,
+				"size": 2000
+			});
+
+			let city = null;
+
+			try {
+				const geoip = request.headers['x-geo-ip'] ?? request.headers['cf-connecting-ip'] ?? request.info.remoteAddress;
+
+				city = lookupIP(geoip)?.city;
+			} catch (e) {
+				console.log(e);
+				//
+			}
+
+			return {
+				current: [(<LocationRecord>city?.location) ? (<LocationRecord>city?.location).latitude : 52.9044, (<LocationRecord>city?.location) ? (<LocationRecord>city?.location).longitude : -1.2326],
+				servers: results.hits.hits.map(hit => ({
+					id: hit._id,
+					hostname: hit.fields['hostname'][0],
+					onlinePlayers: hit.fields['onlinePlayers'][0],
+					maxPlayers: hit.fields['maxPlayers'][0],
+					country: hit.fields['country'][0],
+					location: hit.fields['ipLocation'][0].coordinates.reverse(),
+					distance: hit.sort[0],
+				}))
+			};
+		},
+	});
+
 	router.route({
 		method: 'GET',
 		path: '/statistics/current/network.asn',
@@ -114,6 +191,52 @@ export const routes: RouterFn = (router: Server): void => {
 					averagePlayers: avgPlayers,
 					globalFullness: onlinePlayers / maxPlayers * 100,
 				};
+			});
+		},
+	});
+
+
+	router.route({
+		method: 'GET',
+		path: '/statistics/tsdb/players',
+		handler: async (): Promise<Lifecycle.ReturnValue> => {
+			const fluxQuery = flux`
+				from(bucket: ${config.get('influxdb.bucket')})
+				|> range(start: -7d, stop: now())
+				|> filter(fn: (r) => r["_measurement"] == "server")
+				|> filter(fn: (r) => r["_field"] == "players")
+				|> aggregateWindow(every: 1h, fn: max, createEmpty: true)
+				|> keep(columns: ["_time", "_measurement", "_value", "country"])
+				|> group(columns: ["_time", "country"])
+				|> sort(columns: ["_time"])
+				|> sum()
+				|> group(columns: ["country"])
+			`;
+
+			return new Promise((resolve, reject) => {
+				const rows: [string, number, string][] = [];
+
+				influxdb.getQueryApi(config.get('influxdb.org')).queryRows(fluxQuery, {
+					next(row: string[], consumer) {
+						const data = consumer.toObject(row);
+						rows.push([data._time, data._value, data.country]);
+					},
+					error(err) {
+						reject(err);
+					},
+					complete() {
+						const data = groupBy(rows, r => r[2]);
+						const times = Object.keys(data);
+
+						resolve(times.map(time => ({
+							country: time,
+							points: data[time].map(e => ({
+								value: e[1],
+								ts: e[0],
+							})),
+						})))
+					},
+				});
 			});
 		},
 	});

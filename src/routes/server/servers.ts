9f8aa273ba43d2fa467149d06edb4856fe1b938c
Luke Thompson <luke@xeonr.io>
2022-06-19 04:04:03 +0100

    chore: add more logging

--- src/routes/server/servers.ts
+++ src/routes/server/servers.ts
@@ -441,8 +441,12 @@ export const routes: RouterFn = (router: Server): void => {
 
 			console.log('GOT HEADERS', request.headers);
 			try {
-				city = lookupIP(request.headers['x-geo-ip'] ?? request.headers['cf-connecting-ip'] ?? request.info.remoteAddress)?.city;
+				const geoip = request.headers['x-geo-ip'] ?? request.headers['cf-connecting-ip'] ?? request.info.remoteAddress;
+				console.log('looking up', geoip)
+
+				city = lookupIP(geoip)?.city;
 			} catch(e) {
+				console.log(e);
 				//
 			}
 
